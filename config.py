84a1fca623a7f4e592eb05a0f159d2da0aa83b8a
Rohith0711 <43059291+Rohith0711@users.noreply.github.com>
2024-08-09 21:33:41 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://madhavi:ammu1509@cluster1.mtgz6wq.mongodb.net/expense_tracker?retryWrites=true&w=majority')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://rohitht:Theu12!@#@cluster1.mtgz6wq.mongodb.net/expense_tracker?retryWrites=true&w=majority')
